aae775b9fdc874476e1056358bf7f88b734925e4
Edward-Mercado <edwardmercado149@gmail.com>
2026-03-11 08:20:38 -0400

    tailwind + daisyui + configuration fixed

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,7 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1 class="">You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-
+  <router-view></router-view>
 </template>
+
+<script setup lang="ts"></script>
 
 <style scoped></style>
